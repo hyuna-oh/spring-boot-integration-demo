9979ba7bd9317807f52d9d14ed8acfe0f67aa75f
hyunaoh <hyunah777790@gmail.com>
2021-02-13 20:18:10 +0900

    Datasource 변경

--- src/main/java/com/example/spring/SpringBootIntegrationDemoApplication.java
+++ src/main/java/com/example/spring/SpringBootIntegrationDemoApplication.java
@@ -1,10 +1,13 @@
 package com.example.spring;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootIntegrationDemoApplication {
+@Slf4j
+public class SpringBootIntegrationDemoApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootIntegrationDemoApplication.class, args);
---
+++ src/main/java/com/example/spring/config/DatasourceConfig.java
@@ -0,0 +1,22 @@
+package com.example.spring.config;
+
+import com.zaxxer.hikari.HikariDataSource;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+@Slf4j
+public class DatasourceConfig {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource.hikari")
+    public HikariDataSource dataSource() {
+        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+    }
+
+}
